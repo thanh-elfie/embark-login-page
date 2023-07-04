b422d3174b408ad5bc1f18a3badc6ed453c0f003
Thanh Nguyen <thanh@elfie.co>
2023-07-04 16:12:42 +0700

    Parse redirect url

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,22 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
+import queryString from "query-string";
 
 const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
+  padding: 96
 }
 
 const IndexPage: React.FC<PageProps> = () => {
+  const handleGoToElfie = () => {
+    const params = queryString.parse(location.search);
+    const redirectBaseUri = params.redirect
+    const redirectLink = `${redirectBaseUri}/embark?action=authen&token=${params.token}`
+    const completeRedirectUrl = `${redirectBaseUri}?amv=${params.amv}&apn=${params.apn}&ibi=${params.apn}&imv=0&link=${encodeURIComponent(redirectLink)}`
+    window.location.replace(completeRedirectUrl)
+  }
   return (
     <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Hello world
-      </h1>
+      <button onClick={handleGoToElfie}>Go to Elfie</button>
     </main>
   )
 }
